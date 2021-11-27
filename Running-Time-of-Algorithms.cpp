c4ad4387b69d39b227c72dbf5a1312ffe587aae2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-11-27 15:10:18 +0900

    Update Running-Time-of-Algorithms.cpp

--- Running-Time-of-Algorithms.cpp
+++ Running-Time-of-Algorithms.cpp
@@ -1,1 +1,22 @@
+int runningTime(vector<int> arr)
+{
+    int n = arr.size();
+    int time_count = 0;
+    
+    for (int i = 1; i < n; ++i)
+    {
+        int key = arr[i];
+        int j;
+
+        for (j = i-1; j >= 0 && arr[j] > key; --j)
+        {
+            arr[j+1] = arr[j];
+            time_count++;
+        }
+
+        arr[j+1] = key;
+    }
+
+    return time_count;
+}
 
