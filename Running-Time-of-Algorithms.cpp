584b74c2c568a95247e2e6491db8b39c600a4d22
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-22 07:50:07 +0900

    Update Running-Time-of-Algorithms.cpp

--- Running-Time-of-Algorithms.cpp
+++ Running-Time-of-Algorithms.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'runningTime' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 int runningTime(vector<int> arr)
 {
     int running_time = 0;
