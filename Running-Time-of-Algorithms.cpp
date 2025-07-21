c6aedfdcd6df113113c4b7255172a35d270b156b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-22 07:51:31 +0900

    Update Running-Time-of-Algorithms.cpp

--- Running-Time-of-Algorithms.cpp
+++ Running-Time-of-Algorithms.cpp
@@ -13,28 +13,25 @@ vector<string> split(const string &);
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
-int runningTime(vector<int> arr)
-{
-    int running_time = 0;
+int runningTime(vector<int> arr) {
+    int shifts = 0;
     int n = arr.size();
-    
-    for (int i = 1; i < n; ++i)
-    {
-        int key = arr[i];
-        int j;
 
-        for (j = i-1; j >= 0 && arr[j] > key; --j)
-        {
-            arr[j+1] = arr[j];
-            running_time++;
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            --j;
+            ++shifts;
         }
 
-        arr[j+1] = key;
+        arr[j + 1] = key;
     }
 
-    return running_time;
+    return shifts;
 }
-
 
 int main()
 {
@@ -67,3 +64,44 @@ int main()
     return 0;
 }
 
+string ltrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+    );
+    return s;
+}
+
+std::vector<string> split(const string &str)
+{
+    vector<string> tokens;
+    string_view str_view(str);
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str_view.find(' ', start)) != string_view::npos)
+    {
+        if (end > start)
+        {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
+    }
+
+    if (start < str_view.size())
+    {
+        tokens.emplace_back(str_view.substr(start));
+    }
+
+    return tokens;
+}
