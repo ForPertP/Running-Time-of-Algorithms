4438e3cd9e77fb8d6c8cdd4fc6f3dc1dbfb25720
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-22 07:51:13 +0900

    Update Running-Time-of-Algorithms.cpp

--- Running-Time-of-Algorithms.cpp
+++ Running-Time-of-Algorithms.cpp
@@ -35,3 +35,35 @@ int runningTime(vector<int> arr)
     return running_time;
 }
 
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = runningTime(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
