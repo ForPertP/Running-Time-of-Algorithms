0f5baeb9c986a158201f0fb16d3a18ab85a0b396
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-23 05:49:46 +0900

    Update Running-Time-of-Algorithms.cs

--- Running-Time-of-Algorithms.cs
+++ Running-Time-of-Algorithms.cs
@@ -22,6 +22,30 @@ class Result
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
+    public static int runningTime(List<int> arr)
+    {
+        int shifts = 0;
+        int n = arr.Count;
+
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+                shifts++;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        return shifts;
+    }
+}
+
 
 class Solution
 {
