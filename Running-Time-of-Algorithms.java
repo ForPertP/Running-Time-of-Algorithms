e541a154c01e6530fdb05cd13533d6a2a2945add
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-24 05:27:47 +0900

    Update Running-Time-of-Algorithms.java

--- Running-Time-of-Algorithms.java
+++ Running-Time-of-Algorithms.java
@@ -1,1 +1,41 @@
-Running-Time-of-Algorithms
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'runningTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int runningTime(List<Integer> arr) {
+        int shifts = 0;
+        int n = arr.size();
+
+        for (int i = 1; i < n; i++) {
+            int key = arr.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
+                j--;
+                shifts++;
+            }
+
+            arr.set(j + 1, key);
+        }
+
+        return shifts;
+    }
+}
