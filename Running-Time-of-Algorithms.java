fa80ab3a4983d2d1490140b3f3fd8e1d28878c33
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-24 05:28:00 +0900

    Update Running-Time-of-Algorithms.java

--- Running-Time-of-Algorithms.java
+++ Running-Time-of-Algorithms.java
@@ -39,3 +39,25 @@ class Result {
         return shifts;
     }
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
